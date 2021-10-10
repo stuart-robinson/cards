76b4887dc81ceb81b744a3868f3e9e833efea9ea
Stuart Robinson <stuart@getpenfold.com>
2021-10-10 20:39:05 +0100

    mv to ts

--- src/index.tsx
+++ src/index.tsx
@@ -1,87 +1,92 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 
-const suits = ["club", "heart", "diamond", "spade"];
+enum CardSuit {
+  Clubs = "clubs",
+  Diamonds = "diamonds",
+  Hearts = "hearts",
+  Spades = "spades",
+}
 
-const cardValues = [
-  "ace",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "10",
-  "jack",
-  "queen",
-  "king",
-];
+enum CardRank {
+  Ace = "ace",
+  Two = "2",
+  Three = "3",
+  Four = "4",
+  Five = "5",
+  Six = "6",
+  Seven = "7",
+  Eight = "8",
+  Nine = "9",
+  Ten = "10",
+  Jack = "jack",
+  Queen = "queen",
+  King = "king",
+}
 
-const CardPlaceholder = () => (
+type Card = {
+  suit: CardSuit;
+  rank: CardRank;
+};
+
+type Deck = Array<Card>;
+type Hand = Array<Card>;
+type GameState = { deck: Deck; hand: Hand };
+
+//UI Elements
+const CardBackImage = () => (
   <img src={process.env.PUBLIC_URL + `/SVG-cards/png/1x/back.png`} />
 );
 
-const Card = ({ suit, value }) => {
-  const card = value === "ace" ? 1 : value;
+const CardImage = ({ suit, rank }: Card) => {
+  const card = rank === CardRank.Ace ? 1 : rank;
   return (
     <img
-      src={process.env.PUBLIC_URL + `/SVG-cards/png/1x/${suit}_${card}.png`}
+      src={
+        process.env.PUBLIC_URL +
+        `/SVG-cards/png/1x/${suit.slice(0, -1)}_${card}.png`
+      }
     />
   );
 };
 
-class Game extends React.Component {
-  constructor(props) {
-    super(props);
-    let cards = [];
-    for (let s = 0; s < suits.length; s++) {
-      for (let c = 0; c < cardValues.length; c++) {
-        cards.push({ suit: suits[s], value: cardValues[c] });
-      }
-    }
+//Setup
+const newDeck = (): Deck =>
+  Object.values(CardSuit)
+    .map((suit) =>
+      Object.values(CardRank).map((rank) => ({
+        suit,
+        rank,
+      }))
+    )
+    .reduce((a, v) => [...a, ...v]);
 
-    this.state = {
-      deck: cards,
-      dealt: [],
-    };
-  }
+const initialState: GameState = {
+  deck: newDeck(),
+  hand: [],
+};
 
-  handleDeal = () => {
-    const { deck, dealt } = this.state;
-    dealt.push(deck.pop());
-    this.setState({ deck: deck, dealt: dealt });
-  };
+//Actions
+const dealCardToHand = (state: GameState): GameState => {
+  const [card, ...remaining] = state.deck;
+  return { ...state, deck: remaining, hand: [...state.hand, card] };
+};
 
-  handleShuffle = () => {
-    console.log("shuffle not implemented");
-  };
-
-  render() {
-    const { deck, dealt } = this.state;
-    const isDeckEmpty = deck.length === 0;
-    return (
-      <>
-        <div>
-          <CardPlaceholder />
-          {dealt.map((c) => {
-            return <Card suit={c.suit} value={c.value} />;
-          })}
-          <p>Cards left: {deck.length}</p>
-        </div>
-        <div>
-          <button onClick={this.handleShuffle} disabled={isDeckEmpty}>
-            Shuffle
-          </button>
-          <button onClick={this.handleDeal} disabled={isDeckEmpty}>
-            Deal
-          </button>
-        </div>
-      </>
-    );
-  }
-}
+//Game Component
+const Game = (): JSX.Element => {
+  const [state, setState] = useState(initialState);
+  return (
+    <>
+      <div>
+        <p>There are {state.deck.length} cards left in deck</p>
+        <button onClick={(): void => setState(dealCardToHand)}>Click Me</button>
+      </div>
+      <div>
+        <p>Dealt Cards</p>
+        {state.hand.map(CardImage)}
+      </div>
+    </>
+  );
+};
 
 ReactDOM.render(<Game />, document.getElementById("root"));
